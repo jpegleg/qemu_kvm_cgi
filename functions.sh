092c14bb79670c7049fcd8c14fa557ac94afe9c7
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-02-20 00:29:35 -0700

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -5,7 +5,7 @@ trx() {
 }
 
 yellRetry() {
-  # curl your alerting system here
+  # curl your alerting system here || logme
 }
 
 pullSession() {
@@ -16,34 +16,47 @@ pullSession() {
 exportSession() {
   export machineSession="$( echo $($func) ) | head -n1 | grep started | cut -d' ' -f2)"
 }
-  
+
+storeSession() {
+  echo -n "$sessionTicket $machineSession" |  redis-cli -set -x $sessionTicket
+}  
+
 cloneUbuStable() {
-  virt-clone --original ubuntu20 --auto-clone | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
+  virt-clone --original ubuntu20 --auto-clone --name $sessionTicket | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
 }
 
 cloneDebStable() {
-  virt-clone --original deb10 --auto-clone | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
+  virt-clone --original deb10 --auto-clone --name $sessionTicket | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
 }
 
 cloneFreeBSDStable() {
-  virt-clone --original freebsd12 --auto-clone | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
+  virt-clone --original freebsd12 --auto-clone --name $sessionTicket | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
 }
 
 cloneSuseStable() {
-  virt-clone --original leap15 --auto-clone | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry 
+  virt-clone --original leap15 --auto-clone --name $sessionTicket | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry 
 }
 
 cloneKaliRolling() {
-  virt-clone --original kalirollng --auto-clone | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
+  virt-clone --original kalirollng --auto-clone --name $sessionTicket | grep successfully | cut -d' ' -f2 | xargs virsh start || yellRetry
+}
+
+destroySession() {
+  virsh destroy $sessionRef || yellRetry
+  rm -rf /mnt/kvm/"$sessionTicket".qcow2
 }
 
 retryFail() {
   sleep 1 && makeSession || yellRetry
 }
 
-makeSession() {
-  pullSession || retryFail
-  exportSession || retryFail
+logme() {
+  logger "$(date +%Y%m%d%H%M%S) bad exit code for $sessionTicket $machineSession ERROR $?"
 }
 
-  
+makeSession() {
+  pullSession || logme
+  exportSession || retryFail
+  storeSession || logme
+  echo $machineSession
+}
